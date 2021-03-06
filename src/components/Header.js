6480ac09b71a513e2c770bb191b92c42df2e58c1
Joy Lee <joylee.dev@gmail.com>
2021-03-06 12:10:03 +0900

    테마모드 설정

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,10 @@ export default class Header extends BaseComponent {
     super(`
       <header>
         <h1>JIBSA</h1>
-        <button class="theme-button">테마변경</button>
+        <div class="theme-button">
+          <input type="checkbox" id="theme" />
+          <span></span>
+        </div>
         <div class="search-box"></div>
         <ul class="search-history"></ul>
       </header>
@@ -19,9 +22,48 @@ export default class Header extends BaseComponent {
     const input = new SearchInput(onSearch);
     input.attachTo(searchContainer);
 
-    const button = new RandomButton(onRandom);
-    button.attachTo(searchContainer);
+    const randomButton = new RandomButton(onRandom);
+    randomButton.attachTo(searchContainer);
 
     $target.appendChild(this.element);
+    this.render();
+  }
+
+  setTheme(themeName) {
+    const body = document.querySelector('body');
+    localStorage.setItem('theme', themeName);
+    body.className = themeName;
+  }
+
+  changeThemeOnClick(themeButton) {
+    // localStorage 저장
+    themeButton.addEventListener('click', () => {
+      if (localStorage.getItem('theme') === 'theme-dark') {
+        this.setTheme('theme-light');
+      } else {
+        this.setTheme('theme-dark');
+      }
+    });
+  }
+
+  renderCurrentTheme() {
+    const userPrefersDark =
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    console.log(userPrefersDark);
+    if (localStorage.getItem('theme') === 'theme-dark') {
+      this.setTheme('theme-dark');
+      this.element.querySelector('#theme').checked = true;
+    } else {
+      this.setTheme('theme-light');
+      this.element.querySelector('#theme').checked = false;
+    }
+  }
+
+  render() {
+    const themeButton = this.element.querySelector('.theme-button');
+    this.changeThemeOnClick(themeButton);
+    this.renderCurrentTheme();
   }
 }
